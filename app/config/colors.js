aa04d0d9fc43739d1481d31850f8aff8bc139b49
Kallus878 <kallussydney88@gmail.com>
2025-10-21 22:30:46 -0500

    Added ELTR Colors
    Added new main ELTR colors & commented out some old ones

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,15 +1,29 @@
 export default {
   primary: "#ea9c60", //eltrorange
   secondary: "#8acff4", //eltrblue
-  eltrgreen: "#768e6a",
-  eltrpink: "#e499b7",
-  eltryellow: "#ebc88e",
-  eltrred: "#c34e58",
+  
+  // NEW ELTR main colors
+  eltrred: "#F43545",
+  eltryellow: "#FAD717",
+  eltrpink: "#F786AA",
+  eltrgreen: "#00BA71",
+  eltrpurple: "#5F2879",
+  eltrorange: "#ea9c60",
+  eltrblue: "#8acff4",
+
+
+  // OLD ELTR main colors
+  // eltrgreen: "#768e6a",
+  // eltrpink: "#e499b7",
+  // eltryellow: "#ebc88e",
+  // eltrred: "#c34e58",
+  // eltrpurple: "#800080",
+
+  // OLD ELTR colors
   eltrlightpink: "#FABED2",
   eltrdarkblue: "#6191ab",
   eltrlightblue: "#BCE4F9",
   eltrapricot:"#F9D2BD",
-  eltrpurple: "#800080",
   black: "#000",
   white: "#fff",
   medium: "#6e6969",
