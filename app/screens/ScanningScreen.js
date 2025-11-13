afef6268d7ca6e15d7195e6f079dbeb77ccd1dcf
Kallus878 <kallussydney88@gmail.com>
2025-11-12 19:02:20 -0600

    camera zoom fix

--- app/screens/ScanningScreen.js
+++ app/screens/ScanningScreen.js
@@ -58,6 +58,7 @@ function ScanningScreen({ navigation }) {
           style={StyleSheet.absoluteFill}
           facing="back"
           onBarcodeScanned={barcodeScanned}
+          zoom={0.1} // prevents 0.5 camera effect
         />
       )}
 
