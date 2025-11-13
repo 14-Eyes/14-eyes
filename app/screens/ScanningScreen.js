932db13790ebb4a64fe0b857aadca149b0de158c
Kallus878 <kallussydney88@gmail.com>
2025-11-12 18:45:18 -0600

    Removed unnecessary code from scanning screen

--- app/screens/ScanningScreen.js
+++ app/screens/ScanningScreen.js
@@ -47,21 +47,9 @@ function ScanningScreen({ navigation }) {
       requestPermission();
     }
   }, [permission]);
-  // useEffect(() => {
-  //   (async () => {
-  //     const { status } = await Camera.requestCameraPermissionsAsync();
-  //     setHasPermission(status === "granted");
-  //   })();
-  // }, []);
 
   if (!permission) return <View />;
   if (!permission.granted) return <Text>No access to camera</Text>;
-  // if (hasPermission === null) {
-  //   return <View />;
-  // }
-  // if (hasPermission === false) {
-  //   return <Text>No access to camera</Text>;
-  // }
 
  return (
     <View style={styles.container}>
