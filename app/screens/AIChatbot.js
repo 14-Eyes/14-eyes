291fa5d156c6375bb89c69e18177e3135ac54a65
Josh-29 <joshua.munoz556@gmail.com>
2026-02-02 14:28:38 -0600

    in progress stuff for the chatbot

---
+++ app/screens/AIChatbot.js
@@ -0,0 +1,8 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppText from "../components/AppText";
+import Screen from "../components/Screen";
+import AppButton from "../components/AppButton";
+import colors from "../config/colors";
+import { geminiModel } from "../config/firebase";
+import { getAuth } from "firebase/auth";
+
