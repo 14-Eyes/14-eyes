029ecc82927f34e301d5779e45ea06d46931e5a2
Aayushma <146149566+Aayushma78@users.noreply.github.com>
2026-03-04 15:26:29 -0600

    updates

--- babel.config.js
+++ babel.config.js
@@ -13,8 +13,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // plugins: ['module:react-native-dotenv'],
-    // new plugin configuration to fix white screen error
     plugins: ['react-native-reanimated/plugin'],
   };
 };
